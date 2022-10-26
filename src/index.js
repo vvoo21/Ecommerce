6391185c41c22d70258e616d95437575cd6487a0
Vanessa O <vaneolipa@gmail.com>
2022-10-26 09:38:02 -0500

    Add style to mobile version

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 import './style.css';
+
+const menuIcon = document.querySelector('.icon-menu');
+const modalnavbar = document.querySelector('.modal-navbar-bg');
+const closeIcon = document.querySelector('.modal-navbar-close');
+
+const displayModalNavbar = () => {
+  modalnavbar.style.display = 'flex';
+}
+
+const closeModalNavbar = () => {
+  modalnavbar.style.display = 'none';
+}
+
+menuIcon.addEventListener('click', displayModalNavbar)
+closeIcon.addEventListener('click', closeModalNavbar)
+
